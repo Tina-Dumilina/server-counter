438d54277d7eaaf823676ca72ace23a6ee4f3ff1
Тина Думилина <tina@rnd.stcnet.ru>
2020-10-27 15:48:46 +0300

    Added error page routing

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.use(express.static('public'));
+app.use("/", express.static("public"));
 
-const counter = {
-  counter: 0
-}
+let counter = 0;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get("/api/counter", (req, res) => {
+  res.json({ counter });
+});
 
-app.get('/api/counter', (req, res) => {
-  res.json(counter)
-})
+app.post("/api/counter", (req, res) => {
+  counter++;
+  res.json({ counter });
+});
 
-app.post('/api/counter', (req, res) => {
-  counter.counter += 1
-  res.json(counter)
-})
+app.delete("/api/counter", (req, res) => {
+  counter = 0;
+  res.json({ counter });
+});
 
-app.delete('/api/counter', (req, res) => {
-  counter.counter = 0
-  res.json(counter)
-})
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "error.html"));
+});
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
